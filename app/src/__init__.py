1a7864264b9af5fed6b54ce4af7152ca837b0606
Ertyas54 <167956620+Ertyas54@users.noreply.github.com>
2026-04-23 01:13:19 +0400

    Revert "Add CI/CD pipeline, tests, and linter"
    This reverts commit e71bae7c4d96e11ae3336388728eaa510ad31f2a.

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
-from .models import db
-from .config import Config
-from .routes import health_bp, task_bp
-
+from models import db
+from config import Config
+from routes import health_bp, task_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
     db.init_app(app)
-
+    
     app.register_blueprint(health_bp)
     app.register_blueprint(task_bp)
-
+    
     with app.app_context():
         db.create_all()
-
+    
     return app
--- app/src/routes/__init__.py
+++ app/src/routes/__init__.py
@@ -1,4 +1,2 @@
 from .health_routes import health_bp
 from .task_routes import task_bp
-
-__all__ = ['health_bp', 'task_bp']
